b6bd84ab5a28423e4a2e3ca16d94fb2445573d33
Bion Howard <bion@bitpharma.com>
2020-01-17 21:46:20 -0500

    leverage health check to enable working test in docker-compose

--- www/postcss.config.js
+++ www/postcss.config.js
@@ -1,23 +1,43 @@
-// medium.com/@xijo/create-react-app-with-tailwind-via-postcss-plus-purgecss-5c36b4c33ba7
-
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
-  ],
-  css: ["./src/css/tailwind.css"],
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
-
-//postcss.config.js
-const tailwindcss = require("tailwindcss");
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.config.js"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-    require("cssnano")
+    "tailwindcss",
+    process.env.NODE_ENV === "production"
+      ? [
+          "@fullhuman/postcss-purgecss",
+          {
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}"
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        ]
+      : undefined,
+    "autoprefixer",
+    "cssnano"
   ]
 };
+
+// // medium.com/@xijo/create-react-app-with-tailwind-via-postcss-plus-purgecss-5c36b4c33ba7
+
+// const purgecss = require("@fullhuman/postcss-purgecss")({
+//   content: [
+//     "./pages/**/*.{js,jsx,ts,tsx}",
+//     "./components/**/*.{js,jsx,ts,tsx}",
+//     "./public/index.html"
+//   ],
+//   css: ["./css/tailwind.css"],
+//   // Include any special characters you're using in this regular expression
+//   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+// });
+
+// //postcss.config.js
+// const tailwindcss = require("tailwindcss");
+// module.exports = {
+//   plugins: [
+//     tailwindcss("./tailwind.config.js"),
+//     "autoprefixer",
+//     ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+//     "cssnano"
+//   ]
+// };
