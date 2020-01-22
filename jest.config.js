e4cbf6d7630f3fe755b06fe107a02d7782850827
Bion Howard <bion@bitpharma.com>
2020-01-22 04:44:54 -0500

    fix(ci): gitignore .next and remove from cache

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  setupFiles: ["<rootDir>/jest.setup.js"],
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/tests/e2e",
-    "<rootDir>/.next/"
-  ]
-};
