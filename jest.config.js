9240c0fc9ddccc68c17a82c8615ebe0cf1292840
Bion Howard <bion@bitpharma.com>
2020-01-22 04:11:58 -0500

    fix(unit): jest ignore tests/e2e instead of tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   setupFiles: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: [
     "<rootDir>/node_modules/",
-    "<rootDir>/tests/",
+    "<rootDir>/tests/e2e",
     "<rootDir>/.next/"
   ]
 };
