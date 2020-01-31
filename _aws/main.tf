256bf67fbe1b8378232e562b04e610895ffd0a8e
Bion Howard <bion@bitpharma.com>
2020-01-31 18:59:36 -0500

    add main.tf

---
+++ _aws/main.tf
@@ -0,0 +1,10 @@
+# why?: deploy 3 tier app
+
+# vpc
+# private subnet
+# public subnet
+# aurora postgres
+# elastic beanstalk env
+# elastic beanstalk app (green)
+# elastic beanstalk app (blue)
+# route53 alias
