e532faf8edafe8c79643ee744ba653df80921cda
Bion Howard <bion@bitpharma.com>
2020-02-10 17:38:46 -0500

    fix(tf): try aurora-postgresql engine_Version 10.7

--- _cloud/_terraform/main.tf
+++ _cloud/_terraform/main.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "ouroboros" {
 # aws rds describe-db-engine-versions --query "DBEngineVersions[].DBParameterGroupFamily" | grep postgresql
 resource "aws_rds_cluster_parameter_group" "force_ssl" {
   name   = "database"
-  family = "aurora-postgresql11"
+  family = "aurora-postgresql10.7"
   parameter {
     name         = "rds.force_ssl"
     value        = "1"
@@ -87,13 +87,13 @@ resource "aws_rds_cluster_parameter_group" "force_ssl" {
 resource "aws_rds_cluster" "aurora_serverless_postgresql" {
   engine         = "aurora-postgresql"
   engine_mode    = "serverless"
-  engine_version = "3.0"
+  engine_version = "10.7"
   # enabled_cloudwatch_logs_exports = ["audit"]
-  enable_http_endpoint            = false # "data api"
-  master_username                 = var.db_master_user
-  master_password                 = var.db_master_pass
-  vpc_security_group_ids          = [aws_security_group.ouroboros.id]
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.force_ssl.name
+  enable_http_endpoint   = false # "data api"
+  master_username        = var.db_master_user
+  master_password        = var.db_master_pass
+  vpc_security_group_ids = [aws_security_group.ouroboros.id]
+  # db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.force_ssl.name
   scaling_configuration {
     auto_pause               = true
     seconds_until_auto_pause = 300
