5347ccf618b1af6e9c30eeb2a4bb6d8623c7183a
Bion Howard <bion@bitpharma.com>
2020-02-17 12:50:56 -0500

    fix(ci): don't use docker to run the e2e test

--- pages/api/users/verify.js
+++ pages/api/users/verify.js
@@ -1,6 +1,9 @@
 import { setSession } from "tools/session";
 import db from "tools/db/query";
 
+const verifyQuery =
+  "update app.users set email_verified = true where email = $1 and code = $2 returning *";
+
 /**
  * @path {POST} /users/verify
  * @memberof Users
@@ -12,18 +15,12 @@ import db from "tools/db/query";
  * });
  * verifyResponse.status === 200;
  * ```
- * @body {string} email
- * @body {string} code
- * @code {400} Invalid request
- * @code {500} Server error
- * @code {200} Success
+ * @body {string} email - to verify
+ * @body {string} code - from inbox
  * @returns Secure HTTP-Only JWT Session Cookie
  */
 const verify = db.query({
-  parse: ({ body: { email, code } }) => [
-    "update app.users set email_verified = true where email = $1 and code = $2 returning *",
-    [email, code]
-  ],
+  parse: ({ body: { email, code } }) => [verifyQuery, [email, code]],
   respond: async ({ result: { rows }, res }) => {
     if (rows.length === 0) throw Error("Verification failed.");
     const resWithSession = await setSession(res, rows[0]);
--- tests/e2e/codecept.conf.js
+++ tests/e2e/codecept.conf.js
@@ -1,6 +1,7 @@
-// require("dotenv").config();
+require("dotenv").config();
 
 console.log("CODECEPT CONF JS TEST_URL", process.env.TEST_URL);
+const endpoint = `http://${process.env.TEST_URL}/api`;
 
 // const executablePath = require("child_process").execSync(
 //   "which chromium-browser",
@@ -21,57 +22,57 @@ exports.config = {
       }
     },
     REST: {
-      endpoint: `${process.env.TEST_URL}/api`,
+      endpoint,
       onRequest: request => {
         request.headers.auth = "123";
       }
-    },
-    autoLogin: {
-      enabled: true,
-      saveToFile: true,
-      inject: "signin",
-      users: {
-        super: {
-          login: async I => {
-            await I.sendPostRequest(
-              `/users/${process.env.TEST_SUPER_DISPLAY_NAME}/signin`,
-              { password: process.env.TEST_SUPER_PASSWORD }
-            );
-          },
-          check: I => {
-            I.amOnPage("/");
-            I.see(process.env.TEST_SUPER_DISPLAY_NAME);
-            I.seeCookie("session");
-          }
-        },
-        admin: {
-          login: async I => {
-            await I.sendPostRequest(
-              `/users/${process.env.TEST_ADMIN_DISPLAY_NAME}/signin`,
-              { password: process.env.TEST_ADMIN_PASSWORD }
-            );
-          },
-          check: I => {
-            I.amOnPage("/");
-            I.see(process.env.TEST_ADMIN_DISPLAY_NAME);
-            I.seeCookie("session");
-          }
-        },
-        viewer: {
-          login: async I => {
-            await I.sendPostRequest(
-              `/users/${process.env.TEST_VIEWER_DISPLAY_NAME}/signin`,
-              { password: process.env.TEST_VIEWER_PASSWORD }
-            );
-          },
-          check: I => {
-            I.amOnPage("/");
-            I.see(process.env.TEST_SUPER_DISPLAY_NAME);
-            I.seeCookie("session");
-          }
-        }
-      }
     }
+    // autoLogin: {
+    //   enabled: true,
+    //   saveToFile: true,
+    //   inject: "signin",
+    //   users: {
+    //     super: {
+    //       login: async I => {
+    //         await I.sendPostRequest(
+    //           `/users/${process.env.TEST_SUPER_DISPLAY_NAME}/signin`,
+    //           { password: process.env.TEST_SUPER_PASSWORD }
+    //         );
+    //       },
+    //       check: I => {
+    //         I.amOnPage("/");
+    //         I.see(process.env.TEST_SUPER_DISPLAY_NAME);
+    //         I.seeCookie("session");
+    //       }
+    //     },
+    //     admin: {
+    //       login: async I => {
+    //         await I.sendPostRequest(
+    //           `/users/${process.env.TEST_ADMIN_DISPLAY_NAME}/signin`,
+    //           { password: process.env.TEST_ADMIN_PASSWORD }
+    //         );
+    //       },
+    //       check: I => {
+    //         I.amOnPage("/");
+    //         I.see(process.env.TEST_ADMIN_DISPLAY_NAME);
+    //         I.seeCookie("session");
+    //       }
+    //     },
+    //     viewer: {
+    //       login: async I => {
+    //         await I.sendPostRequest(
+    //           `/users/${process.env.TEST_VIEWER_DISPLAY_NAME}/signin`,
+    //           { password: process.env.TEST_VIEWER_PASSWORD }
+    //         );
+    //       },
+    //       check: I => {
+    //         I.amOnPage("/");
+    //         I.see(process.env.TEST_SUPER_DISPLAY_NAME);
+    //         I.seeCookie("session");
+    //       }
+    //     }
+    //   }
+    // }
   },
   include: {
     I: "./steps_file.js"
@@ -86,5 +87,6 @@ exports.config = {
     screenshotOnFail: {
       enabled: true
     }
-  }
+  },
+  require: []
 };
